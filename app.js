050257de4a83130de6d8fe570bd7f597c6ed1eb8
THATSMEPS <pspratham1234@gmail.com>
2025-03-22 19:58:38 +0530

    Updated app.js

--- app.js
+++ app.js
@@ -29,7 +29,7 @@ io.on("connection", function (socket) {
 app.get ("/", function(req, res) {
     res.render("index");
 });
-
-server.listen(3000, () => {
-    console.log("Server running on http://localhost:3000");
+const PORT = process.env.PORT || 3000;
+server.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
 });
